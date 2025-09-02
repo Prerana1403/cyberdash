15d1d0a1dda9602fda1f6d85e25e667c6f272226
Prerana Polapragada <95952368+Prerana1403@users.noreply.github.com>
2025-09-02 16:45:39 +0530

    Delete cyberdash-main directory

--- cyberdash-main/create_db.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from models import db
-import models.user_model
-import models.ping_model
-import models.nmap_model
-
-with app.app_context():
-    db.create_all()
-    print("All tables created in cyberdash.db")
--- cyberdash-main/models/nmap_model.py
+++
@@ -1,9 +0,0 @@
-from models import db
-from datetime import datetime
-
-class NmapResult(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    target = db.Column(db.String(100), nullable=False)
-    flags = db.Column(db.String(100), nullable=False)
-    result = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
